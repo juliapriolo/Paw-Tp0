93967f1b56bf550216871d8fa556425bd547a4a0
juliapriolo <juliapriolo9@gmail.com>
2026-03-15 16:29:51 -0300

    Funciona jetty

--- paw2026/webapp/src/main/java/ar/edu/itba/paw/webapp/config/WebConfig.java
+++ paw2026/webapp/src/main/java/ar/edu/itba/paw/webapp/config/WebConfig.java
@@ -1,12 +1,25 @@
 package ar.edu.itba.paw.webapp.config;
 
+import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.ComponentScan;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.web.servlet.ViewResolver;
 import org.springframework.web.servlet.config.annotation.EnableWebMvc;
+import org.springframework.web.servlet.view.InternalResourceViewResolver;
+import org.springframework.web.servlet.view.JstlView;
 
 @EnableWebMvc
 @ComponentScan({"ar.edu.itba.paw.webapp.controller"})
 @Configuration
 public class WebConfig {
+
+    @Bean
+    public ViewResolver viewResolver(){
+        final InternalResourceViewResolver vr = new InternalResourceViewResolver();
+        vr.setViewClass(JstlView.class);
+        vr.setPrefix("/WEB-INF/");
+        vr.setSuffix(".jsp");
+        return vr;
+    }
 
 }
